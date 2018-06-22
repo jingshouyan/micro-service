5300d60e7915fc710ad4d041be78e6a24d60f3dd
jingshouyan <290173092@qq.com>
2018-06-22 21:48:25 +0800

    user constant

--- user/src/main/java/io/jing/server/user/constant/UserConstant.java
+++ user/src/main/java/io/jing/server/user/constant/UserConstant.java
@@ -10,14 +10,14 @@ import io.netty.util.AttributeKey;
  * @date 2018/4/24 21:27
  */
 public interface UserConstant extends ServerConstant {
-    public static final String DS_DRIVER = ConfigSettings.get("datasource.driver").get();
-    public static final String DS_USERNAME = ConfigSettings.get("datasource.username").get();
-    public static final String DS_PASSWORD = ConfigSettings.get("datasource.password").get();
-    public static final String DS_URL= ConfigSettings.get("datasource.url").get();
-    public static final int WS_PORT = ConfigSettings.get("ws.port").map(Integer::parseInt).orElse(8888);
-    public static final String WS_URI = "/ws";
-    public static final String API_URI = "/api";
-    public static final String RESOURCE_PATH = "META-INF/resources";
-    public static final String Ticket = "ticket";
-    public static final AttributeKey<Token> TOKEN_KEY = AttributeKey.valueOf("user.token");
+    String DS_DRIVER = ConfigSettings.get("datasource.driver").get();
+    String DS_USERNAME = ConfigSettings.get("datasource.username").get();
+    String DS_PASSWORD = ConfigSettings.get("datasource.password").get();
+    String DS_URL= ConfigSettings.get("datasource.url").get();
+    int WS_PORT = ConfigSettings.get("ws.port").map(Integer::parseInt).orElse(8888);
+    String WS_URI = "/ws";
+    String API_URI = "/api";
+    String RESOURCE_PATH = "META-INF/resources";
+    String Ticket = "ticket";
+    AttributeKey<Token> TOKEN_KEY = AttributeKey.valueOf("user.token");
 }
