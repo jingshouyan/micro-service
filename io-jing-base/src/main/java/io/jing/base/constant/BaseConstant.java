da68e6a51e60d7c4ff9d1e72d09f52e08e3291c9
jingshouyan <290173092@qq.com>
2018-07-12 19:00:12 +0800

    log path

--- io-jing-base/src/main/java/io/jing/base/constant/BaseConstant.java
+++ io-jing-base/src/main/java/io/jing/base/constant/BaseConstant.java
@@ -11,7 +11,7 @@ public interface BaseConstant {
     String ZK_ADDRESS = ConfigSettings.get("zk.address").orElse("127.0.0.1:2181");
     String THRIFT_SERVER_NAME = ConfigSettings.get("thrift.server.name").orElse("microService");
     String THRIFT_SERVER_VERSION = ConfigSettings.get("thrift.server.version").orElse("1.0");
-    String LOG_ROOT_PATH = ConfigSettings.get("log.root.path").orElse("/data/java/logs");
+    String LOG_ROOT_PATH = ConfigSettings.get("log.root.path").orElse("/data/logs");
     String LOG_LEVEL = ConfigSettings.get("log.level").orElse("DEBUG");
     String LOG_REF = ConfigSettings.get("log.ref").orElse("STDOUT");
     String TRACE_ID = "TRACE_ID";
