e0630bb19334377ad3b495d1701fe56876c1ba33
Jing Shouyan <290173092@qq.com>
2018-09-11 09:43:53 +0800

    findBugs

--- acl/src/test/java/io/jing/server/acl/method/ObjectTest.java
+++ acl/src/test/java/io/jing/server/acl/method/ObjectTest.java
@@ -1,5 +1,8 @@
 package io.jing.server.acl.method;
 
+import io.jing.base.util.json.JsonUtil;
+import io.jing.server.acl.bean.ResourceBean;
+
 import java.net.URLClassLoader;
 
 public class ObjectTest {
@@ -19,9 +22,27 @@ public class ObjectTest {
     }
 
     public static void main(String[] args) {
-        ObjectTest objectTest = new ObjectTest();
-        ObjectTest objectTest1 = new ObjectTest();
-        ObjectTest objectTest2 = new ObjectTest();
-        URLClassLoader classLoader;
+//        ObjectTest objectTest = new ObjectTest();
+//        ObjectTest objectTest1 = new ObjectTest();
+//        ObjectTest objectTest2 = new ObjectTest();
+//        URLClassLoader classLoader;
+
+        ResourceBean resourceBean = new ResourceBean();
+        resourceBean.setName("资源更新");
+        resourceBean.setCode("RESOURCE_UPDATE");
+        resourceBean.setDescription("");
+        resourceBean.setMethod("POST");
+        resourceBean.setUri("/acl/resource/update");
+        resourceBean.setType(1);
+        resourceBean.setState(1);
+        resourceBean.setLogout(false);
+        resourceBean.setCreatedAt(0L);
+        resourceBean.setUpdatedAt(0L);
+        resourceBean.setDeletedAt(0L);
+
+        String str = JsonUtil.toJsonString(resourceBean);
+
+        System.out.println(str);
+
     }
 }
